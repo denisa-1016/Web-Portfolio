93a97f5f701cc8ecbb0b4ecb63c7373201f9779d
Soare Denisa <denisasoare1016@gmail.com>
2025-09-26 22:11:00 +0300

    The rest of the files.

---
+++ web-portfolio/src/app/page.jsx
@@ -0,0 +1,23 @@
+import Navbar from "@/app/components/Navbar";
+import styles from "./page.module.css";
+import Image from "next/image";
+import profilePic from "./Poza.png";
+
+export default function Home(){
+    return (
+        <main>
+            <Navbar />
+            <section id="home" className={styles.container}>
+                <div className={styles.left}>
+                    <h1>Hi There! I'm Denisa</h1>
+                    <h1>Welcome to my website!</h1>
+                    <p>I'm a tech passionate! I love working with modern technologies Next.js and creating projects.</p>
+                </div>
+
+                <div className={styles.right}>
+                    <Image src={profilePic} alt="Thats me!"></Image>
+                </div>
+            </section>
+        </main>
+    );
+}
